cc271784b158adbeb56c5317cea641feedc14dcd
Youfang6 <m19970566094@163.com>
2024-04-12 21:16:39 +0800

    增加了一个hello git

--- src/main/java/com/student/Main.java
+++ src/main/java/com/student/Main.java
@@ -11,6 +11,8 @@ package com.student;
  */
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("hello git");
     }
 }
