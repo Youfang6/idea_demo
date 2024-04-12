8d9db2ab5ff4240b4fcb253041b03e0267ce40aa
Youfang6 <m19970566094@163.com>
2024-04-12 20:52:53 +0800

    hello git

---
+++ src/main/java/com/student/Hello.java
@@ -0,0 +1,16 @@
+package com.student;
+
+/**
+ * ClassName: Hello
+ * Package: com.student
+ * Description:
+ *
+ * @Author YCLRun
+ * @Create 2024/4/12 20:51
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, git!");
+    }
+}
